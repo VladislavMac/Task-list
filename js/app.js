49c3411a0b4cdac628fde4593d6d606738bfb820
VladislavMac <mvvminsk@gmail.com>
2023-01-11 22:52:11 +0300

    add local storage and fix little bugs

--- js/app.js
+++ js/app.js
@@ -7,34 +7,52 @@ const cleaningInput = input =>{
     input.value = '';
 }
 
-    // Add new task
-buttonNewTask.addEventListener('click', () => {
+function makeTask(text, date){
+    const parent = document.querySelector('.wrapper-content-tasks');
+
+        // set variables
+    const objTask            = new Task(text, date, parent);
+          task               = objTask.task,
+          taskRemoveButton   = task.querySelector('.wrapper-content-tasks-task-header-remove_button'),
+          taskCrossoutButton = task.querySelector('.wrapper-content-tasks-task-header-crossout_button');
+
+        // set local storage 
+    localStorage.setItem('Task=' + date, text)
+
+        // set functionality for buttons
+    taskRemoveButton.addEventListener('click', ()=>{
+        objTask.remove();
+    })
+    taskCrossoutButton.addEventListener('click', ()=>{
+        objTask.crossout();
+    })
+
+        // render task
+    objTask.render();
+
+    cleaningInput(inputEnterTask);
+    cleaningInput(inputEnterDate);
+}
+
+function setLocalStorageTasks(){
+    for( let i = (localStorage.length - 1); i >= 0; i--){
+        if( localStorage.key(i).indexOf('Task') != -1 ){
+
+            const date = localStorage.key(i).split('=')[1],
+                  text = localStorage.getItem(localStorage.key(i))
+                  
+            makeTask(text, date)
+        }
+    }
+}
+
+setLocalStorageTasks()
+
+buttonNewTask.addEventListener('click', () =>{
     const text    = inputEnterTask.value,
           date    = inputEnterDate.value;
 
-    const parent = document.querySelector('.wrapper-content-tasks');
-
     if( text.length != 0 ){
-
-            // set variables
-        const objTask            = new Task(text, date, parent);
-        const task               = objTask.task;
-        const taskRemoveButton   = task.querySelector('.wrapper-content-tasks-task-header-remove_button');
-        const taskCrossoutButton = task.querySelector('.wrapper-content-tasks-task-header-crossout_button')
-
-            // set functionality for buttons
-        taskRemoveButton.addEventListener('click', ()=>{
-            objTask.remove();
-        })
-        taskCrossoutButton.addEventListener('click', ()=>{
-            objTask.crossout();
-        })
-        
-            // render task
-        objTask.render();
-
-        cleaningInput(inputEnterTask);
-        cleaningInput(inputEnterDate);
+        makeTask(text,date)
     }
 });
-
